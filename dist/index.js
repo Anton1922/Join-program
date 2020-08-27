497b26d384c9ceed650a36c60e887c0a63520601
Anton Fedorov <anton.pzkt@gmail.com>
2020-08-27 23:48:07 +0300

    final files and images

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var form = document.querySelector('.mailpoet_form');
+var inputName = form.querySelector('input[type="text"].mailpoet_text');
+var inputEmail = form.querySelector('input[type="email"].mailpoet_text');
+inputName.placeholder = 'Ваше имя';
+inputEmail.placeholder = 'E-mail';
+//# sourceMappingURL=index.js.map
