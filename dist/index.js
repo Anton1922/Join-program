567b43143d545ea88571427881f8c707683dede8
Anton Fedorov <anton.pzkt@gmail.com>
2020-09-01 15:40:31 +0300

    final files

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,8 @@
 var form = document.querySelector('.mailpoet_form');
 var inputName = form.querySelector('input[type="text"].mailpoet_text');
 var inputEmail = form.querySelector('input[type="email"].mailpoet_text');
-inputName.placeholder = 'Ваше имя';
-inputEmail.placeholder = 'E-mail';
+var buttonContainer = form.querySelector('.mailpoet_paragraph.last');
+inputName.placeholder = 'Введите имя';
+inputEmail.placeholder = 'Электронная почта';
+buttonContainer.insertAdjacentHTML = ('beforeend', '<div class="join-subscribe__privacy-policy">Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c <a href="https://less-homeless.com//wp-content/uploads/2020/02/Политика-конфиденциальности.docx" class="join-subscribe__privacy-policy-link" target="_blank" rel="noopener noreferrer">политикой конфиденциальности</a></div>');
 //# sourceMappingURL=index.js.map
